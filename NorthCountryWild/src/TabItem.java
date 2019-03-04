953173b8bc51196fd0018d5c5a6c29df3d75995e
guineveregilman <35609986+guineveregilman@users.noreply.github.com>
2019-03-04 11:42:24 -0500

    Added a panel instance variable to the TabItem class that I forgot to include

--- NorthCountryWild/src/TabItem.java
+++ NorthCountryWild/src/TabItem.java
@@ -1,3 +1,4 @@
+import java.swing.JPanel;
 
 // parent class for a panel in the tab
 public class TabItem {
@@ -5,11 +6,13 @@ public class TabItem {
 	private String desc;
 	private String hint;
 	private String returnValue;
+	private JPanel panel;
 	
 	public TabItem(String desc, String hint, String rv) {
 		this.desc = desc;
 		this.hint = hint;
 		this.returnValue = rv;
+		panel = new JPanel();
 	}
 	
 	public String getDesc() {
