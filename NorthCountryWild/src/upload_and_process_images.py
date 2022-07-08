71a8af24824f2b38e1db668c5134743ef12a3609
Brett Ford <brett.michael.ford@gmail.com>
2022-07-08 10:38:23 -0400

    BF- add user friendly prompts to all subcommands

--- NorthCountryWild/src/upload_and_process_images.py
+++ NorthCountryWild/src/upload_and_process_images.py
@@ -22,6 +22,7 @@ import shutil
 import subprocess
 from tkinter import Tk, ttk
 from tkinter.filedialog import askdirectory
+from typing import Union
 
 import exiftool
 from wand.api import library
@@ -68,7 +69,25 @@ class wimage(Image):
         return library.MagickSetOption(self.wand, binary(key), binary(value))
 
 
-def change_file_size_and_copyright(path_to_processed_images: str) -> None:
+def change_file_size_and_copyright(
+    path_to_processed_images: str,
+) -> None:
+    def get_processed_images_dir():
+        global processed_images_dir
+        processed_images_dir = askdirectory(title="CHOOSE PROCESSED IMAGES DIR")
+        win.destroy()
+
+    if path_to_processed_images is None:
+        win = Tk()
+        win.title("Process North Country Wild Photos")
+        win.geometry("600x300")
+        b2 = ttk.Button(
+            win, text="CHOOSE PROCESSED IMAGES DIR", command=get_processed_images_dir
+        ).pack(side="top", padx=50, pady=120)
+        win.mainloop()
+        path_to_processed_images = processed_images_dir
+
+    print(path_to_processed_images)
     for filename in os.listdir(path_to_processed_images):
         # Only process unhidden images
         if not filename.startswith("."):
@@ -94,8 +113,6 @@ def change_file_size_and_copyright(path_to_processed_images: str) -> None:
 
 
 def copy_raw_images_change_file_size_and_copyright(
-    path_to_raw_images: str,
-    path_to_processed_images: str,
     camera_number_with_leading_zeros: str,
     sd_card_number_with_leading_zeros: str,
 ) -> None:
@@ -104,18 +121,44 @@ def copy_raw_images_change_file_size_and_copyright(
     camera_number_with_leading_zeros = camera_number_with_leading_zeros.zfill(3)
     sd_card_number_with_leading_zeros = sd_card_number_with_leading_zeros.zfill(3)
 
+    def get_raw_images_dir():
+        global raw_images_dir
+        raw_images_dir = askdirectory(title="CHOOSE RAW IMAGES DIR")
+        win.destroy()
+
+    def get_processed_images_dir():
+        global processed_images_dir
+        processed_images_dir = askdirectory(title="CHOOSE PROCESSED IMAGES DIR")
+        win.destroy()
+
+    win = Tk()
+    win.title("Process North Country Wild Photos")
+    win.geometry("600x300")
+    b1 = ttk.Button(win, text="CHOOSE RAW IMAGES DIR", command=get_raw_images_dir).pack(
+        side="top", padx=50, pady=120
+    )
+    win.mainloop()
+
+    win = Tk()
+    win.title("Process North Country Wild Photos")
+    win.geometry("600x300")
+    b2 = ttk.Button(
+        win, text="CHOOSE PROCESSED IMAGES DIR", command=get_processed_images_dir
+    ).pack(side="top", padx=50, pady=120)
+    win.mainloop()
+
     # Copy and rename images from raw_images_path to processed_images_path
-    for filename in os.listdir(path_to_raw_images):
+    for filename in os.listdir(raw_images_dir):
         # Only process unhidden images
         if not filename.startswith("."):
-            raw_filepath = os.path.join(path_to_raw_images, filename)
+            raw_filepath = os.path.join(raw_images_dir, filename)
             filename_with_prefix = f"C{camera_number_with_leading_zeros}_SD{sd_card_number_with_leading_zeros}_{filename}"
             processed_filepath = os.path.join(
-                path_to_processed_images, filename_with_prefix
+                processed_images_dir, filename_with_prefix
             )
             shutil.copy(raw_filepath, processed_filepath)
 
-    change_file_size_and_copyright(path_to_processed_images)
+    change_file_size_and_copyright(processed_images_dir)
 
 
 def completely_process_images_from_sd_card(
@@ -142,18 +185,20 @@ def completely_process_images_from_sd_card(
         win.destroy()
 
     win = Tk()
-    b1 = ttk.Button(
-        win, text="CHOOSE RAW IMAGES DIR", command=get_raw_images_dir
-    ).pack()
+    win.title("Process North Country Wild Photos")
+    win.geometry("600x300")
+    b1 = ttk.Button(win, text="CHOOSE RAW IMAGES DIR", command=get_raw_images_dir).pack(
+        side="top", padx=50, pady=120
+    )
     win.mainloop()
-    print(raw_images_dir)
 
     win = Tk()
+    win.title("Process North Country Wild Photos")
+    win.geometry("600x300")
     b2 = ttk.Button(
         win, text="CHOOSE PROCESSED IMAGES DIR", command=get_processed_images_dir
-    ).pack()
+    ).pack(side="top", padx=50, pady=120)
     win.mainloop()
-    print(processed_images_dir)
 
     # Copy images from memory_card_path to raw_images_path
     for filename in os.listdir(memory_card_path):
@@ -186,34 +231,22 @@ def get_args():
 
     file_size_and_copyright_parser = subparsers.add_parser(
         "change_file_size_and_copyright",
-        help="this subcommand takes a path to the processed images which have already been renamed and resizes them and adds copyright information",
+        help="this subcommand takes a path to the processed images, which have already been renamed, and resizes them and adds copyright information",
     )
     file_size_and_copyright_parser.set_defaults(func=change_file_size_and_copyright)
     file_size_and_copyright_parser.add_argument(
         "--path-to-processed-images",
         type=str,
-        required=True,
+        required=False,
         help="full path to where processed images are held; assumes you want to overwrite files here",
     )
 
     copy_raw_image_file_size_and_copyright_parser = subparsers.add_parser(
         "copy_raw_images_change_file_size_and_copyright",
-        help="this subcommand takes a path to the raw images along with where processed images will be and the camera number and sd number, as three-digit integers, and copies, resizes, and adds copyright information to the images",
+        help="this subcommand takes the camera number and sd number, as three-digit integers, and copies, resizes, and adds copyright information to the images",
     )
     copy_raw_image_file_size_and_copyright_parser.set_defaults(
         func=copy_raw_images_change_file_size_and_copyright
-    )
-    copy_raw_image_file_size_and_copyright_parser.add_argument(
-        "--path-to-raw-images",
-        type=str,
-        required=True,
-        help="full path to raw images, before renaming, resizing, or changing copyright",
-    )
-    copy_raw_image_file_size_and_copyright_parser.add_argument(
-        "--path-to-processed-images",
-        type=str,
-        required=True,
-        help="full path to where processed images will be placed",
     )
     copy_raw_image_file_size_and_copyright_parser.add_argument(
         "--camera-number-with-leading-zeros",
