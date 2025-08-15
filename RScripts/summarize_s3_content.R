8b29c748d6a65de0169b253398894481f9ba7b65
Erika Barthelmess <barthelmess@stlawu.edu>
2025-08-15 17:43:52 -0400

    write scripts to summarize what has been uploaded on s3

---
+++ RScripts/summarize_s3_content.R
@@ -0,0 +1,3 @@
+#This function reads the .txt file with this function takes a summarized data frame as its argument and returns a list of two nice-looking tables.
+
+#copy function over from return_list_of_deployments_on_s3.Qmd and then add code there to source, simplfy that code.
